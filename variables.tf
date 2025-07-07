af7d8310920fee68b1f3e6ef4416bbc404b51819
umamaheshdega@umadega <mahesh445667@gmail.com>
2025-07-07 10:21:17 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "project_name" {
+
+}
+
+variable "environment" {
+
+}
+
+variable "sg_description" {
+
+}
+
+variable "vpc_id" {
+
+}
+
+variable "sg_name" {
+
+}
+
+variable "common_tags" {
+    type = map
+
+}
+
+variable "sg_tags" {
+    default = {}
+}
